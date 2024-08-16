5ac6e33d9bccc034f5caa448256fdcbab393fe14
Cata Puñales <cpunalesblanc@gmail.com>
2024-08-16 20:20:36 -0300

    Finnn

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener ("DOMContentLoaded", function() {
+const Div = document.querySelector("div");
+
+Div.addEventListener ("click", function(){
+    alert("Hola soy el div");
+});
+})
