c2feb55decd454f9992196deb7dc24b220298e62
Cata Puñales <cpunalesblanc@gmail.com>
2024-08-16 21:06:39 -0300

    Solucion

--- eventos.js
+++ eventos.js
@@ -1,7 +1,11 @@
 document.addEventListener ("DOMContentLoaded", function() {
 const Div = document.querySelector("div");
+const Boton = document.querySelector ("button")
 
 Div.addEventListener ("click", function(){
     alert("Hola soy el div");
 });
+Boton.addEventListener ("click", function(event) {
+    event.stopPropagation();
+})
 })
